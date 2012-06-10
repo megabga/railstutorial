67352b94cedd44c81b17009ca2ece4de9d782726
megabga <megabga@gmail.com>
2012-06-10 10:24:12 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Railstutorial::Application.routes.draw do
+  get "users/new"
+
   #get "static_pages/home" <- convert to named route wirh MATCH
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
 
   root to: 'static_pages#home' 
+  
+  match '/signup',  to: 'users#new'
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
